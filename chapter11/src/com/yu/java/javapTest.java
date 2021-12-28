38c4e143fde8caa69968af53ce322154a1223a30
Yzhiqiang <2595889669@qq.com>
2021-12-28 22:51:56 +0800

    5

---
+++ chapter11/src/com/yu/java/javapTest.java
@@ -0,0 +1,18 @@
+package com.yu.java;
+
+/**
+ * @Author:Yuzhiqiang
+ * @Description:
+ * @Date: Create in 19:03 2021/12/28
+ * @Modified By:
+ */
+public class javapTest {
+    public void Print() {
+        Integer I_value = 10;
+        System.out.println(I_value);
+    }
+    public static void main(String[] args) {
+        javapTest javapTest = new javapTest();
+        javapTest.Print();
+    }
+}
